d4fed6dcc83366f7b57462abe1101354dce50eac
Malin Pinsky <malin.pinsky@gmail.com>
2025-10-09 15:02:06 -0700

    Fix to avoid junior synonyms.

--- readchecklist.R
+++ readchecklist.R
@@ -24,36 +24,31 @@ for(i in 1:length(lines)){
 }
 
 head(out)
-nrow(out) # 239 species found the Albatross
+nrow(out) # 239 species described from the Albatross collection. Some are junior synonyms.
 
 
 
 ## Second, find Philippine endemics
 status <- read.csv('zookeys-1246-001_article-145752__-s001.csv', skip =1) # read in Table S1
 
-status$sppname <- status$Original.ScientificName # step one to pick a valid name
-replacename <- which(status$ValidAs.Remarks != "") # find lines with "valid as" entries
-status$sppname[replacename] <- status$ValidAs.Remarks[replacename] # replace valid name with "valid as" entry
-status$sppname_short <- sapply(strsplit(status$sppname, "\\s+"), function(y) paste(head(y, 2), collapse = " ")) # get only first two words
-status$sppname_short_oneword <- gsub(" ", "", status$sppname_short) # remove the space
+status$sppname <- sapply(strsplit(status$Original.ScientificName, "\\s+"), function(y) paste(head(y, 2), collapse = " ")) # get only first two words
 
 ve_lines <- which(status$Status == "VE") # get lines with Philippines endemics (marked "VE")
-endemics <- status[ve_lines, c("Original.ScientificName", "Status", "ValidAs.Remarks", "sppname", "sppname_short", "sppname_short_oneword")]
+endemics <- status[ve_lines, c("Original.ScientificName", "Status", "ValidAs.Remarks", "sppname")]
 
 nrow(endemics) # 221 endemics
 
 
 
 ## Third, find Philippine endemics found by the Albatross
-endemics2[] <- lapply(endemics, function(x) { # remove all spaces, since the list of Albatross holotypes is missing spaces
-    if (is.character(x)) gsub(" ", "", x) else x
-})
+validrows <- endemics$ValidAs.Remarks == "" # find lines (T/F) without "valid as" entries. Having an entry implies the original name is a junior synonym, and not valid.
+endemicspp <- gsub(" ", "", endemics$sppname) # set up a list endemics to match against. No spaces since the Albatross holotypes is missing spaces.
 
 pattern <- paste(out$species, collapse = "|") # long search pattern of all Albatross holotypes
-Albatross_endemic_lines <- apply(endemics2, 1, function(row) any(grepl(pattern, row, ignore.case = TRUE))) # find lines in endemics2 that are Albatross holotypes
+Albatross_endemic_lines <- grepl(pattern, endemicspp, ignore.case = TRUE) & validrows # find entries in endemicspp that are Albatross holotypes and valid as senior synonyms
 
-sum(Albatross_endemic_lines) # finds 28 species
-Albatross_endemics2 <- endemics$sppname_short[Albatross_endemic_lines] # list of the species, using their valid name
+sum(Albatross_endemic_lines) # finds 14 species
+Albatross_endemics <- endemics$sppname[Albatross_endemic_lines] # list of the species
 
 ## Write out
-write.csv(endemics[Albatross_endemic_lines, c("Original.ScientificName", "ValidAs.Remarks", "sppname_short")], file = "Albatross_endemics.csv", row.names = FALSE)
+write.csv(endemics[Albatross_endemic_lines, c("Original.ScientificName", "ValidAs.Remarks", "sppname")], file = "Albatross_endemics.csv", row.names = FALSE)
